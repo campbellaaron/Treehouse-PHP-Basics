b56dbc38e2bfee6637455ccb004a5b20e868df63
campbellaaron <jenovawitness@hotmail.com>
2017-05-02 14:36:41 -0400

    PHP Committed

---
+++ conditionals.php
@@ -0,0 +1,40 @@
+<?php 
+$a = 6;
+$b = 5;
+
+// if ($a == $b) {
+//   echo "Values are equal \n";
+// } else {
+//   echo "Values are not equal \n";
+// }
+
+// $score = 60;
+// if ($score >= 59) {
+//   echo "You completed the level!";
+// } else {
+//   echo "Please try again!";
+// }
+
+// if ($a) {
+//   echo 'True';
+// }
+
+// if (!$a) {
+//   echo 'false';
+// }
+
+$num = '10';
+
+// if ($num >= 10 && $num <= 1000) {
+//   echo "Your number is within the range";
+// } else {
+//   echo "Your number is NOT within the range";
+// }
+
+if ($num == 10 || is_string($num)) {
+  echo '10 or string';
+} else {
+  echo 'NOT 10 or string';
+}
+
+?>
